deb3b71a17f9d29b4267942099da1e9efca28ff4
Kevin Che <cheyuany@usc.edu>
2020-08-06 01:12:35 -0700

    Resolve static pages

--- FinalProject/src/main/java/com/controller/GreetingController.java
+++ FinalProject/src/main/java/com/controller/GreetingController.java
@@ -4,14 +4,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
-public class GreetingController {
+public class GreetingController extends AbstractController {
 
     @GetMapping("greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting.jsp";
+    }
+
+    @GetMapping("login")
+    public String login() {
+        return "loginPage.html";
+    }
+
+    @GetMapping("index")
+    public String query() {
+        return "index.jsp";
+    }
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
+
+        ModelAndView model = new ModelAndView("HelloWorldPage");
+        model.addObject("msg", "hello world");
+
+        return model;
     }
 
 }
