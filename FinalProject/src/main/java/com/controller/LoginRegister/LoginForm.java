de2bd37306b3bbe7491bfe84e0bf835638b0412c
Kevin Che <cheyuany@usc.edu>
2020-08-06 15:13:27 -0700

    Login/Register/Guest

--- FinalProject/src/main/java/com/controller/LoginRegister/LoginForm.java
+++ FinalProject/src/main/java/com/controller/LoginRegister/LoginForm.java
@@ -1,19 +1,20 @@
-package com.controller.LoginForm;
+package com.controller.LoginRegister;
 
 public class LoginForm {
-    private String username;
+
+    private String email;
     private String password;
 
     public LoginForm() {
         super();
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
