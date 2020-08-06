a82d485b701254b31238e2d6a97df0aa95c0ad22
djleng <dleng@usc.edu>
2020-08-06 01:04:57 -0700

    apiClient set apiToken

--- demo/src/main/java/com/example/demo/Test.java
+++ demo/src/main/java/com/example/demo/Test.java
@@ -7,7 +7,12 @@ import java.math.BigDecimal;
 
 public class Test {
     public static void main(String[] args) {
+        ApiClient apiClient = new ApiClient();
+        apiClient.setApiKey("apiKey=612a8ebcab0b449b8316b61349cc769e");
+
         DefaultApi apiInstance = new DefaultApi();
+        apiInstance.setApiClient(apiClient);
+        System.out.println(apiInstance.getApiClient().getAuthentications());
         Boolean limitLicense = true; // Boolean | Whether the recipes should have an open license that allows display with proper attribution.
         String tags = "vegetarian, dessert"; // String | The tags (can be diets, meal types, cuisines, or intolerances) that the recipe must have.
         BigDecimal number = BigDecimal.valueOf(1); // BigDecimal | The number of random recipes to be returned (between 1 and 100).
