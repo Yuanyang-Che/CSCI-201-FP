2843e1e44592abbd05914b07f9d4dd8b7a54729b
Yijia Chen <yijiac@usc.edu>
2020-08-05 18:14:51 -0700

    Add files via upload

---
+++ thymeleaf-custom-layout/README.md
@@ -0,0 +1,4 @@
+Thymeleaf Custom Layout
+=======================
+
+See [Thymeleaf template layouts in Spring MVC application with no extensions](http://blog.codeleak.pl/2013/11/thymeleaf-template-layouts-in-spring.html)
---
+++ thymeleaf-custom-layout/src/main/java/org/thymeleaf/spring/support/Layout.java
@@ -0,0 +1,12 @@
+package org.thymeleaf.spring.support;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Layout {
+    String NONE = "none"; // no layout will be used
+
+    String value();
+}
