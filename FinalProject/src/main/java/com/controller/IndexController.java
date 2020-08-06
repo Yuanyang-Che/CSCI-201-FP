8a6fb0c250cac150fd820dd3c81b798aee59c5b8
Kevin Che <cheyuany@usc.edu>
2020-08-06 01:16:56 -0700

    Revert "delete unnecessary files"
    This reverts commit 5472ad8c24249c2d07067965559585e276e7f086.

---
+++ FinalProject/src/main/java/com/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String home(Map<String, Object> model) {
+        model.put("message", "HowToDoInJava Reader !!");
+        return "index";
+    }
+
+    @RequestMapping("/next")
+    public String next(Map<String, Object> model) {
+        model.put("message", "You are in new page !!");
+        return "next";
+    }
+
+}
