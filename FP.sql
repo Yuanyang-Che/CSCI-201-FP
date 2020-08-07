baa7726d3cd1dda2fc0feb509faa790a47ae5ddd
Kevin Che <cheyuany@usc.edu>
2020-08-06 20:31:56 -0700

    Update FP.sql

--- FP.sql
+++ FP.sql
@@ -7,7 +7,7 @@ CREATE TABLE user_info (
 	-- id_token INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
 	email VARCHAR(45) PRIMARY KEY NOT NULL UNIQUE,
     user_name VARCHAR(45) NOT NULL,
-	pw VARCHAR(45) NOT NULL
+	password VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE diet_restriction (
@@ -28,7 +28,7 @@ CREATE TABLE last_recipe (
     FOREIGN KEY fk2(email) REFERENCES user_info(email)
 );
 
-INSERT INTO user_info (email, user_name, pw)
+INSERT INTO user_info (email, user_name, password)
 	VALUE ('test@usc.edu', 'usr1', '123a');
 
 
