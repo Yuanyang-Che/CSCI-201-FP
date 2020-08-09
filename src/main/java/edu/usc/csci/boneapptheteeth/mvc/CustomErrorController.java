fdda27868dc30104c787a2970779b1fc103b4d66
Kevin Che <cheyuany@usc.edu>
2020-08-08 21:34:40 -0700

    Added the custom error page

---
+++ src/main/java/edu/usc/csci/boneapptheteeth/mvc/CustomErrorController.java
@@ -0,0 +1,21 @@
+package edu.usc.csci.boneapptheteeth.mvc;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "customError";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
