4987b0a9bd2434052b2436bf61ccf3890affd80d
Derek Lin <derekjli@usc.edu>
2020-08-10 15:40:12 -0700

    search form changes

--- src/main/java/edu/usc/csci/boneapptheteeth/mvc/EdemamController.java
+++ src/main/java/edu/usc/csci/boneapptheteeth/mvc/EdemamController.java
@@ -2,6 +2,7 @@ package edu.usc.csci.boneapptheteeth.mvc;
 
 import edu.usc.csci.boneapptheteeth.mvc.dto.Recipe;
 import edu.usc.csci.boneapptheteeth.service.EdamamApiService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,10 +14,11 @@ public class EdemamController {
         Recipe recipe = instance.getRandomRecipe();
         return recipe;
     }
-    @GetMapping("/search/{query}")
-    public Recipe getRecipeBySearchJSON(@PathVariable("query") String query) {
+    @GetMapping("/searchData")
+    public Recipe getRecipeBySearchJSON(@RequestParam(name = "search", required = true) String query,  Model model) {
         EdamamApiService instance = new EdamamApiService();
         Recipe recipe = instance.getRecipeBySearch(query);
+        model.addAttribute("recipe", recipe);
         return recipe;
     }
 }
