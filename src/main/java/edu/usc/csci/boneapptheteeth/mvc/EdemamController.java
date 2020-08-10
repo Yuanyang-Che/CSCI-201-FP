23e7ee10a0acbef9e27020428cc67ded382ea327
Kevin Che <cheyuany@usc.edu>
2020-08-10 16:04:40 -0700

    Create recipe.html

--- src/main/java/edu/usc/csci/boneapptheteeth/mvc/EdemamController.java
+++ src/main/java/edu/usc/csci/boneapptheteeth/mvc/EdemamController.java
@@ -3,10 +3,13 @@ package edu.usc.csci.boneapptheteeth.mvc;
 import edu.usc.csci.boneapptheteeth.mvc.dto.Recipe;
 import edu.usc.csci.boneapptheteeth.service.EdamamApiService;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/recipe")
+
 public class EdemamController {
     @RequestMapping(value = "/random", method = RequestMethod.GET)
     public Recipe getRecipeBySearchJSON() {
@@ -14,8 +17,9 @@ public class EdemamController {
         Recipe recipe = instance.getRandomRecipe();
         return recipe;
     }
-    @GetMapping("/searchData")
-    public Recipe getRecipeBySearchJSON(@RequestParam(name = "search", required = true) String query,  Model model) {
+
+    @RequestMapping(value = "/searchData")
+    public Recipe getRecipeBySearchJSON(@RequestParam(name = "search") String query, Model model) {
         EdamamApiService instance = new EdamamApiService();
         Recipe recipe = instance.getRecipeBySearch(query);
         model.addAttribute("recipe", recipe);
